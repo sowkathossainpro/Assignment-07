f27b403bea3a1f93983c61c9e339ddd82095388a
sowkat hossain <sthn031@gmail.com>
2026-02-04 17:27:06 +0600

    Assignment-07 final fixed

--- src/components/tickets/SingleTicket.jsx
+++ src/components/tickets/SingleTicket.jsx
@@ -11,7 +11,7 @@ const SingleTicket = ({ singleData, handleCount }) => {
     return (
         <div className=''>
             <div onClick={handleClick} className='bg-white mb-3 shadow-lg p-4 rounded-2xl border border-gray-200'>
-                <div className='flex justify-between items-center text-xl md:text-sm'>
+                <div className='flex justify-between items-center text-xl md:text-xl sm:text-sm'>
                     <h2 className='font-bold'>{singleData.title}</h2>
                     <h3 className={`py-1 px-5 rounded-2xl ${singleData.status == "Open" ? "text-green-900 bg-green-300" : "text-yellow-900 bg-yellow-300"}`}>
                         {singleData.status === "Open" && (
